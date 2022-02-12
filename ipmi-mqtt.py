160a92412f8a0cfcfc3f4231a87f419119f96f00
Carlos Alfredo Diaz <cdiazjr4@gmail.com>
2022-02-12 03:04:02 -0300

    Update ipmi-mqtt.py
    added unique ID for multiple equal type SDRs in same server

--- ipmi-mqtt.py
+++ ipmi-mqtt.py
@@ -145,7 +145,7 @@ def main():
                     unit = ""
                 server_mqtt_config_topic = ha_sensor_topic + "/" + guid_list[server_number] + "_" + sdr_topic + "/" + "config"
                 server_mqtt_state_topic = ha_sensor_topic + "/" + guid_list[server_number] + "_" + sdr_topic + "/" + "state"     
-                mqtt_payload = {"device_class" : sdr_class, "name" : server_nodename + " " + sdr_topic , "unique_id" : guid_list[server_number], "Unit of Measurement" : unit, "force_update" : True, "payload_on" : "on", "payload_off" : "off" , "retain" : True, "state_topic" : server_mqtt_state_topic }
+                mqtt_payload = {"device_class" : sdr_class, "name" : server_nodename + " " + sdr_topic , "unique_id" : guid_list[server_number] + sdr_number, "Unit of Measurement" : unit, "force_update" : True, "payload_on" : "on", "payload_off" : "off" , "retain" : True, "state_topic" : server_mqtt_state_topic }
                 mqtt_payload = json.dumps(mqtt_payload)  
                 sdr_payload[server_mqtt_config_topic] =  mqtt_payload
                 sdr_number = sdr_number + 1
